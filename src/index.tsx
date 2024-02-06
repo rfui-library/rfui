4c9d122d4c7cdabd219063afcb21692931a1a5bd
Adam Zerner <adamzerner@protonmail.com>
2024-02-05 21:29:01 -0800

    Try moving `Button` to directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,1 @@
-import * as React from "react";
-import { useState } from "react";
-
-export const Button = () => {
-  const [numTimesClicked, setNumTimesClicked] = useState(0);
-  const incrementNumTimesClicked = () => {
-    setNumTimesClicked((n) => n + 1);
-  };
-
-  return (
-    <button onClick={incrementNumTimesClicked}>
-      Clicked {numTimesClicked} times
-    </button>
-  );
-};
+export { Button } from "./atoms/button";
